a3ae16f3a84f449bb79f3aa4d1989271ea995241
martendo <51766598+martendo@users.noreply.github.com>
2021-03-18 11:43:40 -0400

    Loop over extension objects to add them
    This way it's less verbose and extension type values don't have to be
    added in manually.

--- src/end.js
+++ src/end.js
@@ -31,42 +31,24 @@ msgpack.codec.preset.addExtUnpacker(0x00, (buffer) => {
   const properties = msgpack.decode(buffer);
   return new ImageData(properties[0], properties[1], properties[2]);
 });
-// Pos2D
-msgpack.codec.preset.addExtPacker(0x20, Pos2D, (pos) => Pos2D.packer(pos));
-msgpack.codec.preset.addExtUnpacker(0x20, (buffer) => Pos2D.unpacker(buffer));
-// PastAction
-msgpack.codec.preset.addExtPacker(0x21, PastAction, (action) => PastAction.packer(action));
-msgpack.codec.preset.addExtUnpacker(0x21, (buffer) => PastAction.unpacker(buffer));
-// Action
-msgpack.codec.preset.addExtPacker(0x22, Action, (action) => Action.packer(action));
-msgpack.codec.preset.addExtUnpacker(0x22, (buffer) => Action.unpacker(buffer));
-// Stroke
-msgpack.codec.preset.addExtPacker(0x23, Stroke, (stroke) => Stroke.packer(stroke));
-msgpack.codec.preset.addExtUnpacker(0x23, (buffer) => Stroke.unpacker(buffer));
-// Fill
-msgpack.codec.preset.addExtPacker(0x24, Fill, (fill) => Fill.packer(fill));
-msgpack.codec.preset.addExtUnpacker(0x24, (buffer) => Fill.unpacker(buffer));
-// Selection
-msgpack.codec.preset.addExtPacker(0x25, Selection, (selection) => Selection.packer(selection));
-msgpack.codec.preset.addExtUnpacker(0x25, (buffer) => Selection.unpacker(buffer));
-// SelectionResize
-msgpack.codec.preset.addExtPacker(0x26, SelectionResize, (selectionResize) => SelectionResize.packer(selectionResize));
-msgpack.codec.preset.addExtUnpacker(0x26, (buffer) => SelectionResize.unpacker(buffer));
-// OldSelection
-msgpack.codec.preset.addExtPacker(0x27, OldSelection, (old) => OldSelection.packer(old));
-msgpack.codec.preset.addExtUnpacker(0x27, (buffer) => OldSelection.unpacker(buffer));
-// ShortSelection
-msgpack.codec.preset.addExtPacker(0x28, ShortSelection, (shortSel) => ShortSelection.packer(shortSel));
-msgpack.codec.preset.addExtUnpacker(0x28, (buffer) => ShortSelection.unpacker(buffer));
-// Line
-msgpack.codec.preset.addExtPacker(0x29, Line, (line) => Line.packer(line));
-msgpack.codec.preset.addExtUnpacker(0x29, (buffer) => Line.unpacker(buffer));
-// Shape
-msgpack.codec.preset.addExtPacker(0x2A, Shape, (shape) => Shape.packer(shape));
-msgpack.codec.preset.addExtUnpacker(0x2A, (buffer) => Shape.unpacker(buffer));
-// ShapeColours
-msgpack.codec.preset.addExtPacker(0x2B, ShapeColours, (colours) => ShapeColours.packer(colours));
-msgpack.codec.preset.addExtUnpacker(0x2B, (buffer) => ShapeColours.unpacker(buffer));
+[
+  Pos2D,
+  PastAction,
+  Action,
+  Stroke,
+  Fill,
+  Selection,
+  SelectionResize,
+  OldSelection,
+  ShortSelection,
+  Line,
+  Shape,
+  ShapeColours
+].forEach((object, index) => {
+  const type = 0x20 + index;
+  msgpack.codec.preset.addExtPacker(type, object, (obj) => object.packer(obj));
+  msgpack.codec.preset.addExtUnpacker(type, (buffer) => object.unpacker(buffer));
+});
 
 // List of ping latency measurements to calculate average
 var prevPings = [];
